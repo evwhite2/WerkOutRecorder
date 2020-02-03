ea56869b88d744277d72f18f3a88b6c393da52fe
Ellen <evwhite2@gmail.com>
2020-02-02 18:28:21 -0600

    fixing html paths

--- routes/html.js
+++ routes/html.js
@@ -1,25 +1,19 @@
 
 const router = require("express").Router();
-var path = "C:\\Users\\evwhi\\bootcamp\\WerkOutRecorder\\public";
+const path = require("path");
 
 
 router.get("/", (req, res)=>{
-    res.sendFile(path+"index.html", (err, success)=>{
-        if(err) console.log(err);
-    })
+    res.sendFile(path.join(__dirname+ "./../public/index.html"))
 })
 
 
 router.get("/exercise", (req, res)=>{
-    res.sendFile(path+"/exercise.html", (err, success)=>{
-        if(err) console.log(err);
-    })
+    res.sendFile(path.join(__dirname+ "./../public/exercise.html"))
 })
 
 router.get("/stats", (req, res)=>{
-    res.sendFile(path+"/stats.html", (err, success)=>{
-        if(err) console.log(err);
-    })
+    res.sendFile(path.join(__dirname+ "./../public/stats.html"))
 })
 
 
